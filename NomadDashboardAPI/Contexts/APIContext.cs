fe748b110794d3d799390da55ed999c296cc9659
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-12 11:41:09 -0700

    Made some changes in API & User Contexts

--- NomadDashboardAPI/Contexts/APIContext.cs
+++ NomadDashboardAPI/Contexts/APIContext.cs
@@ -1,17 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NomadDashboardAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NomadDashboardAPI.Contexts
 {
     public class APIContext : DbContext
     {
-        public APIContext()
+        public APIContext(DbContextOptions<APIContext> options) : base(options)
         {
-
         }
 
         public DbSet<Lead> Leads { get; set; }
--- NomadDashboardAPI/Contexts/UserContext.cs
+++ NomadDashboardAPI/Contexts/UserContext.cs
@@ -1,20 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using NomadDashboardAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NomadDashboardAPI.Contexts
 {
     public class UserContext : IdentityDbContext
     {
-        public UserContext()
-        {
-        }
 
-        public UserContext(DbContextOptions options) : base(options)
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
         }
 
