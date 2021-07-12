7f78cea678490aad4004c05a14e74753738f9ce2
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-12 11:35:58 -0700

    Added Leads & Project Controllers

---
+++ NomadDashboardAPI/Controllers/LeadController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NomadDashboardAPI.Interfaces;
+using NomadDashboardAPI.Models;
+using System.Collections.Generic;
+
+namespace NomadDashboardAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeadController : ControllerBase
+    {
+        private readonly ILead _leadService;
+
+        public LeadController(ILead leadService)
+        {
+            _leadService = leadService;
+        }
+
+        // Route = /api/Lead/GetAllLeads/
+        [HttpGet]
+        [Route("GetAllLeads")]
+        public ActionResult<IEnumerable<Lead>> GetAllLeads()
+        {
+            var leads = _leadService.GetAllLeads();
+            return Ok(leads);
+        }
+
+        [HttpGet]
+        [Route("GetLeadById/{id}")]
+        public ActionResult<Lead> GetLeadById(string id)
+        {
+            var leads = _leadService.GetLeadById(id);
+            if (leads != null)
+            {
+                return Ok(new { succeeded = true, leads = leads });
+            }
+            else
+            {
+                return Ok(new { succeeded = false, code = "LeadNotFound", description = "Lead with id '" + id + "' Not Found" });
+            }
+        }
+    }
+}
